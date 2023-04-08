a29cdf712fb4af83adbd9640d303129ca8a809ae
georgiy semikolenov <georgiy.semikolenov@mail.ru>
2023-04-08 15:01:21 +0400

    .

--- ServiceStation_Course/ServiceStationDataModels/Models/ISpareModel.cs
+++ ServiceStation_Course/ServiceStationDataModels/Models/ISpareModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServiceStationDataModels.Models
+﻿namespace ServiceStationDataModels.Models
 {
     public interface ISpareModel : IId
     {
--- ServiceStation_Course/ServiceStationDatabaseImplement/Models/Work.cs
+++ ServiceStation_Course/ServiceStationDatabaseImplement/Models/Work.cs
@@ -1,14 +1,8 @@
 ﻿using ServiceStationContracts.BindingModels;
 using ServiceStationContracts.ViewModels;
 using ServiceStationDataModels.Models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServiceStationDatabaseImplement.Models
 {
